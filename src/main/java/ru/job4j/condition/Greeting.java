09c37ebbe3eda53105994ff227e97ad37cbae143
mac <ideer@yandex.ru>
2022-03-29 22:25:10 +0500

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Jawa!";
+        System.out.println(idea);
+        System.out.println(idea + " But I am a newbie.");
+        int year = 2022;
+        String id = idea + year;
+        System.out.println(id);
+    }
+}
